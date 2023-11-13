a238da316d1ee8b57729256b442096545a230499
Indian1999 <haszab99@gmail.com>
2023-11-13 06:33:52 +0100

    codeCleanUp2

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -8,21 +8,22 @@ def remvoveSpecialChars(str):
 def getNumberOfUniqueChars(str):
     uniqueCharList = []
     for char in str:
-        if char.lower() not in uniqueCharList:
+        if char not in uniqueCharList:
             uniqueCharList.append(char.lower())
     return len(uniqueCharList)
         
 def isPalindrome(str):
     str = remvoveSpecialChars(str)
+    str = str.lower()
     noUniqueChars = getNumberOfUniqueChars(str)
     for i in range(0, len(str) // 2):
-        if str[i].lower() != str[-i-1].lower():
+        if str[i] != str[-i-1]:
             return ("NO", -1)
     return ("YES", noUniqueChars)
     
 with open('./input.txt', 'r') as f:
     line = f.readline()
     while line:
-        result = isPalindrome(line)
-        print(f"{result[0]}, {result[1]}")
+        isPalindrome, uniqueChars  = isPalindrome(line)
+        print(f"{isPalindrome}, {uniqueChars}")
         line = f.readline()
